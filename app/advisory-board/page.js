96cc1fd240a36eb4c9ed1d8222b60cf40c33c880
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2025-07-11 19:43:30 +0800

    finish advisory board

--- app/advisory-board/page.js
+++ app/advisory-board/page.js
@@ -1,32 +1,62 @@
-import Image from 'next/image';
+import Image from "next/legacy/image";
 
-const ProfileBox = ({ photoUrl, name, position }) => {
-  return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden max-w-sm w-full transform transition duration-300 hover:scale-105">
-      <div className="relative w-36 h-36 mx-auto mt-4 overflow-hidden rounded-lg">
-        <Image
-          src={photoUrl}
-          alt={name}
-          fill
-          objectFit="cover"
-        />
-      </div>
-      <div className="p-4 text-center">
-        <h2 className="text-2xl font-bold text-gray-800">{name}</h2>
-        <p className="text-gray-600">{position}</p>
-      </div>
-    </div>
-  );
+const ProfileBox = ({ photoUrl, name, positions }) => {
+    return (
+        <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden w-full">
+            <div className="relative w-36 h-48 mx-auto mt-4 overflow-hidden rounded-lg">
+                <Image
+                    src={photoUrl}
+                    alt={name}
+                    layout="fill"
+                    objectFit="cover"
+                />
+            </div>
+            <div className="p-4 text-center">
+                <h2 className="text-lg/8 md:text-xl/8 font-bold dark:text-gray-100">{name}</h2>
+                {positions.map((position, index) => (
+                    <p key={index} className="text-gray-600 dark:text-gray-300 text-sm md:text-base">{position}</p>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default function AdvisoryBoardPage() {
     return (
         <div>
-            <ProfileBox
-                photoUrl="/CHUNGTszShun.jpg"
-                name="Dr. John Doe"
-                position="Chairman, Advisory Board"
-            />
+            <h1 className="text-4xl/8 font-bold mb-4">Advisory Board</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-8">
+                <ProfileBox
+                    photoUrl="/CHUNGTszShun.jpg"
+                    name="Prof. CHUNG Tsz Shun Eric"
+                    positions={["Professor", "Department of Mathematics, Faculty of Science, CUHK"]}
+                />
+                <ProfileBox
+                    photoUrl="/JINBangTi.jpg"
+                    name="Prof. JIN Bang Ti"
+                    positions={["Professor (Global STEM Scholar)", "Department of Mathematics, Faculty of Science, CUHK"]}
+                />
+                <ProfileBox
+                    photoUrl="/LEUNGNaiChung.jpg"
+                    name=" Prof. Conan Nai Chung LEUNG"
+                    positions={["Professor", "Department of Mathematics, Faculty of Science, CUHK"]}
+                />
+                <ProfileBox
+                    photoUrl="/XINZhouPing.jpg"
+                    name=" Prof. XIN Zhou Ping"
+                    positions={["William M.W. Mong Professor of Mathematics", " Executive Director, The Institute of Mathematical Sciences, CUHK"]}
+                />
+                <ProfileBox
+                    photoUrl="/YUJiuKang.jpg"
+                    name="Prof. YU Jiu Kang"
+                    positions={["Lee Hysan Professor of Mathematics", "Department of Mathematics, Faculty of Science, CUHK"]}
+                />
+                <ProfileBox
+                    photoUrl="/ZOUJun.png"
+                    name="Prof. ZOU Jun"
+                    positions={["Choh-Ming Li Professor of Mathematics", "Chairman, Department of Mathematics, Faculty of Science, CUHK"]}
+                />
+            </div>
         </div>
     );
 }
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import { Forum } from "next/font/google";
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 const lobster = Forum({
   subsets: ["latin"],
