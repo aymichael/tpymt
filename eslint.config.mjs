350114c50479c2ba068c6316084983103d70eb78
aymichael <aymichael@gmail.com>
2025-09-17 10:01:48 +0800

    Revert "Update eslint.config.mjs"
    This reverts commit d4577e94feea509fd13c87a82b84705e015f7698.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,16 +9,6 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
-
-  // 在這裡加入 rules 設定
-  {
-    rules: {
-      "react/no-unescaped-entities": "off",
-      // 其他規則關閉或調整可繼續新增
-    },
-  },
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
 export default eslintConfig;
